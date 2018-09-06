b31f52c1a8cfaca273c70be443215b6ac0817876
Clive Walkden <clive@sozodesign.co.uk>
2018-09-06 09:23:46 +0100

    Updated the layout for SOZO for testing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,10 @@
     <packagist position="b1"></packagist>
     <npm position="b2"></npm>
     <github position="b3"></github>
-    <music position="d1"></music>
-    <tasks team-member="alex" position="c1"></tasks>
-    <tasks team-member="brent" position="c2"></tasks>
-    <tasks team-member="freek" position="c3"></tasks>
-    <tasks team-member="seb" position="d2"></tasks>
-    <tasks team-member="willem" position="d3"></tasks>
-    <time-weather position="e1" date-format="ddd DD/MM" time-zone="Europe/Brussels" weather-city="Antwerp"></time-weather>
+    <music position="c1:d1"></music>
+    <tasks team-member="clive" position="c2:c3"></tasks>
+
+    <time-weather position="e1" date-format="ddd DD/MM" time-zone="Europe/London" weather-city="Cheltenham"></time-weather>
     <calendar position="e2:e3"></calendar>
     <internet-connection></internet-connection>
 </dashboard>
